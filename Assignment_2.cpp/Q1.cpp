1921b6032c509441a872e56436b0ec44f16dafcb
FourArms11 <ishaankumar247@gmail.com>
2025-10-25 13:10:38 +0530

    25/10/25

---
+++ Assignment_2.cpp/Q1.cpp
@@ -0,0 +1,59 @@
+// Implement the binary search algorithm regarded as a fast search algorithm
+// with run-time complexity of Ο(log n) in comparison to the Linear Search.
+
+#include <iostream>
+using namespace std;
+
+int *sort(int a[], int n)
+{
+    for (int i = 0; i < n - 1; i++)
+    {
+        for (int j = 0; j < n - 1 - i; j++)
+        {
+            if (a[j] > a[j + 1])
+            {
+                int temp = a[j];
+                a[j] = a[j + 1];
+                a[j + 1] = temp;
+            }
+        }
+    }
+    return a;
+}
+
+void binary_search(int a[], int n, int value)
+{
+    int *ptr = sort(a, n);
+    int start = 0;
+    int end = n - 1;
+    bool found = false;
+
+    while (start <= end)
+    {
+        int mid = start + (end - start) / 2;
+
+        if (ptr[mid] == value)
+        {
+            cout << "Element found at position: " << mid << endl;
+            found = true;
+            break;
+        }
+        else if (value > ptr[mid])
+            start = mid + 1;
+        else
+            end = mid - 1;
+    }
+
+    if (!found)
+        cout << "Element not found" << endl;
+}
+int main()
+{
+    int n = 5;
+    int val;
+    cout << "Enter the value to search: ";
+    cin >> val;
+    int arr[n] = {5, 3, 2, 4, 1};
+
+    binary_search(arr, n, val);
+}
--- Assingment-1/Q5.cpp
+++ Assingment-1/Q5.cpp
@@ -3,6 +3,7 @@ using namespace std;
 int main()
 {
     int arr[3][3];
+    cout<<"enter your array: "<<endl;
     for (int i = 0; i < 3; i++)
     {
         for (int j = 0; j < 3; j++)
