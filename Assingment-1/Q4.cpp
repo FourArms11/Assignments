351fc08d8b51d9df6ddf60dc38d71e7d207d3f8f
FourArms11 <ishaankumar247@gmail.com>
2025-11-27 18:11:52 +0530

    done2

--- Assingment-1/Q4.cpp
+++ Assingment-1/Q4.cpp
@@ -95,3 +95,41 @@ int main()
 }
 
 // c. Find the Transpose of a Matrix
+
+
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int r, c;
+    cout << "Enter rows: ";
+    cin >> r;
+    cout << "Enter columns: ";
+    cin >> c;
+
+    int a[20][20], t[20][20];
+
+    cout << "Enter matrix elements:\n";
+    for (int i = 0; i < r; i++) {
+        for (int j = 0; j < c; j++) {
+            cin >> a[i][j];
+        }
+    }
+
+    for (int i = 0; i < r; i++) {
+        for (int j = 0; j < c; j++) {
+            t[j][i] = a[i][j];
+        }
+    }
+
+    cout << "\nTranspose:\n";
+    for (int i = 0; i < c; i++) {
+        for (int j = 0; j < r; j++) {
+            cout << t[i][j] << " ";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
